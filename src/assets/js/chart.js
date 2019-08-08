71d732903756f415d434cddb6d862dc10bc2428a
Jonah Snider <me@jonahsnider.ninja>
2019-08-08 00:23:22 -0700

    refactor: change "shorten" to "shortened"
    Signed-off-by: Jonah Snider <me@jonahsnider.ninja>

--- src/assets/js/chart.js
+++ src/assets/js/chart.js
@@ -3,7 +3,7 @@ const add = (time, index) => [time, index + 1];
 
 export const update = (chart, data) =>
   chart.updateSeries([
-    { name: "Shorten", data: data.shorten.map(add) },
+    { name: "Shortened", data: data.shorten.map(add) },
     { name: "Visited", data: data.get.map(add) }
   ]);
 
