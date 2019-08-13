aac6e053292d397e330e9a973dd198140c0d80d8
Jonah Snider <me@jonahsnider.ninja>
2019-08-12 22:55:02 -0700

    fix: fix copying clipboard on Safari on iOS
    Signed-off-by: Jonah Snider <me@jonahsnider.ninja>

--- src/assets/js/copy.js
+++ src/assets/js/copy.js
@@ -3,11 +3,23 @@ export default str => {
     return navigator.clipboard.writeText(str);
   } else {
     const el = document.createElement("textarea");
+    const range = document.createRange();
+
     el.classList.add("clipboard");
+    el.contentEditable = true
+    el.readOnly = false;
     el.value = str;
+
     document.body.appendChild(el);
 
-    el.select();
+    range.selectNodeContents(el);
+
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    // In case our URLs become 9007199254740991 2^53 − 1 digits long
+    el.setSelectionRange(0, Number.MAX_SAFE_INTEGER);
+
     document.execCommand("copy");
 
     document.body.removeChild(el);
