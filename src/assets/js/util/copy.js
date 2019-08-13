638a7f1b8a426c59919564ee843214c3ec2c522e
Yousef Sultan <yousef.su.2000@gmail.com>
2019-08-14 02:22:19 +0300

    attempt to solve issues #1
    Signed-off-by: Yousef Sultan <yousef.su.2000@gmail.com>

--- src/assets/js/util/copy.js
+++ src/assets/js/util/copy.js
@@ -1,15 +1,25 @@
 export default str => {
-  console.log('copying', str, navigator);
-  if (navigator && navigator.clipboard) {
-    return navigator.clipboard.writeText(str);
+  const iOS = navigator.userAgent.match(/ipad|iphone/i);
+
+  // Create text area
+  const textArea = document.createElement("textArea");
+  textArea.value = str;
+  document.body.appendChild(textArea);
+
+  // Select the text area. iOS needs a workaround.
+  if (iOS) {
+    const range = document.createRange();
+    range.selectNodeContents(textArea);
+
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    textArea.setSelectionRange(0, 999999);
   } else {
-    const el = document.createElement("textarea");
-    el.value = str;
-    document.body.appendChild(el);
-
-    el.select();
-    document.execCommand("copy");
-
-    document.body.removeChild(el);
+    textArea.select();
   }
+
+  // Trigger copying
+  document.execCommand("copy");
+  document.body.removeChild(textArea);
 };
