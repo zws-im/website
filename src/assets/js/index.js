35742d2f3db1c2f905e8311afc4c829fe55b2f11
Jonah Snider <me@jonahsnider.ninja>
2019-08-13 11:58:26 -0700

    style: organize imports
    Signed-off-by: Jonah Snider <me@jonahsnider.ninja>

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,7 @@
+import ApexCharts from "apexcharts";
 import { apexCharts, elements } from "./constants";
 import shorten from "./events/shorten";
 import stats from "./events/stats";
-import ApexCharts from "apexcharts";
 
 elements.forms.shorten.addEventListener("submit", shorten);
 elements.forms.stats.addEventListener("submit", stats);
