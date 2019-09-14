e97db4534900f5e7fcd4fcff392b9fc17ed1d15a
K4USTUBH <k4ustu3h@gmail.com>
2019-09-14 20:20:18 +0530

    added Firebase

---
+++ src/assets/js/firebaseconfig.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAvrW2ZAkHxhCrQTaepWW8TdZQ1zi5aVJc",
+  authDomain: "kodriga.firebaseapp.com",
+  databaseURL: "https://kodriga.firebaseio.com",
+  projectId: "kodriga",
+  storageBucket: "",
+  messagingSenderId: "700289828537",
+  appId: "1:700289828537:web:4b10493e89c4039078fd68"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
