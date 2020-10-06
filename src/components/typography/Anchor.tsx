66fc63f6954cf57f6492190921778761e0196dac
Jonah Snider <jonah@jonah.pw>
2020-10-05 23:48:20 -0700

    fix(anchor): fix Next.js `Link` breaking with child functional component

--- src/components/typography/Anchor.tsx
+++ src/components/typography/Anchor.tsx
@@ -1,9 +1,9 @@
 import clsx from 'clsx';
-import React, {AnchorHTMLAttributes, FC} from 'react';
+import React, {AnchorHTMLAttributes, FC, MutableRefObject} from 'react';
 import styles from './Anchor.module.scss';
 
 export interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {}
 
-export const Anchor: FC<Props> = ({className, ...props}) => {
-	return <a className={clsx(className, styles.link)} {...props} />;
-};
+export const Anchor: FC<Props> = React.forwardRef(({className, ...props}, ref: MutableRefObject<HTMLAnchorElement>) => (
+	<a className={clsx(className, styles.link)} ref={ref} {...props} />
+));
