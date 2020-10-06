f317e456db262886e22d51b36c616fa8638997e2
Jonah Snider <jonah@jonah.pw>
2020-10-05 23:48:37 -0700

    refactor(nav): rewrite `NavItem` type
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,16 +3,13 @@ import React, {FC, HTMLAttributes, ReactChild} from 'react';
 import styles from './Nav.module.scss';
 import {Anchor} from './typography/Anchor';
 
-export interface Item {
-	key: string;
-	to: ReactChild;
-}
+export type Item = ReactChild;
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
 	children: Item[];
 }
 
-export const Nav: FC<Props> = ({children, ...props}) => {
+export const Nav: FC<Props> = props => {
 	return (
 		<nav className={styles.nav}>
 			<Link passHref href='/'>
@@ -20,9 +17,7 @@ export const Nav: FC<Props> = ({children, ...props}) => {
 					<img className={styles.logo} src='/images/logo.png' />
 				</Anchor>
 			</Link>
-			<div className={styles.links} {...props}>
-				{children.map(item => item.to)}
-			</div>
+			<div className={styles.links} {...props} />
 		</nav>
 	);
 };
--- src/nav.tsx
+++ src/nav.tsx
@@ -5,36 +5,16 @@ import {Anchor} from './components/typography/Anchor';
 import React from 'react';
 
 export const navItems: NavItem[] = [
-	{
-		key: 'stats',
-		to: (
-			<Link key='stats' passHref href='/'>
-				<Anchor>Stats</Anchor>
-			</Link>
-		)
-	},
-	{
-		key: 'questions',
-		to: (
-			<Link key='questions' passHref href={`/#${HomeSectionIds.Faq}`}>
-				<Anchor>Questions</Anchor>
-			</Link>
-		)
-	},
-	{
-		key: 'docs',
-		to: (
-			<Link key='docs' passHref href='/'>
-				<Anchor>Docs</Anchor>
-			</Link>
-		)
-	},
-	{
-		key: 'github',
-		to: (
-			<Anchor key='github' href='https://github.com/zws-im?utm_source=zws.im'>
-				GitHub
-			</Anchor>
-		)
-	}
+	<Link key='stats' passHref href='/'>
+		<Anchor>Stats</Anchor>
+	</Link>,
+	<Link key='questions' passHref href={`/#${HomeSectionIds.Faq}`}>
+		<Anchor>Questions</Anchor>
+	</Link>,
+	<Link key='docs' passHref href='/'>
+		<Anchor>Docs</Anchor>
+	</Link>,
+	<Anchor key='github' href='https://github.com/zws-im?utm_source=zws.im'>
+		GitHub
+	</Anchor>
 ];
