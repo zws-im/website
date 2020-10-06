2001aa6ce42d1523b0d04fefc6b2ba09d817e5b0
Jonah Snider <jonah@jonah.pw>
2020-10-05 23:28:57 -0700

    fix(anchor): stop using weird ref code
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- src/components/typography/Anchor.tsx
+++ src/components/typography/Anchor.tsx
@@ -4,6 +4,6 @@ import styles from './Anchor.module.scss';
 
 export interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {}
 
-export const Anchor: FC<Props> = React.forwardRef(({className, onClick, href, ...props}, ref) => {
-	return <a className={clsx(className, styles.link)} href={href} onClick={onClick} ref={ref} {...props} />;
-});
+export const Anchor: FC<Props> = ({className, ...props}) => {
+	return <a className={clsx(className, styles.link)} {...props} />;
+};
